de3efc8af01df64ead84d4248f4b64dba85812e3
Robson Silva <rm8l@outlook.com>
2023-03-02 19:09:36 -0300

    req 10 e 12

--- app/backend/src/api/interfaces/IUserService.ts
+++ app/backend/src/api/interfaces/IUserService.ts
@@ -5,7 +5,7 @@ interface IUserService {
   // userLogin(user: IUser): Promise<IToken>
   accValidation(email: string, password: string): Promise<IToken | null>;
   getRole(email: string): Promise<IUser | null>;
-  // passwordValidation(password: string, user: IUser): Promise<null | string>
+  // passwordValidation()
 }
 
 export default IUserService;
