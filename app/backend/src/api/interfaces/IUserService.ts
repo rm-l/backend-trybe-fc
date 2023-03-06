02110e132d8ea17216fd44eb6d2ede6086c56d01
Robson Silva <rm8l@outlook.com>
2023-03-06 14:38:11 -0300

    remove comments +

--- app/backend/src/api/interfaces/IUserService.ts
+++ app/backend/src/api/interfaces/IUserService.ts
@@ -2,10 +2,8 @@ import IUser from './IUser';
 import IToken from './IToken';
 
 interface IUserService {
-  // userLogin(user: IUser): Promise<IToken>
   accValidation(email: string, password: string): Promise<IToken | null>;
   getRole(email: string): Promise<IUser | null>;
-  // passwordValidation()
 }
 
 export default IUserService;
